92dc91bc1612da668d217fa26393a44d5becfa64
Alex Francois <apfrancois86@gmail.com>
2016-12-20 16:49:55 -0800

    add url variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+let url= ["index.html","services.html","about.html", ""]
+
 $(document).ready(function(){
   $(".top-scroll").click(function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
